bf7974cb74ce2f0a27e1488d51fab1be11fb9e5e
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2024-03-13 18:03:43 +0200

    chore: initialized self hosted runner script

---
+++ uninstall-self-hosted-runner.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+if [ ! -z $DEBUG ]; then
+    set -ex
+fi
+
+GITHUB_REPOSITORY=${GITHUB_REPOSITORY:-invalid}
+GITHUB_TOKEN=${GITHUB_TOKEN:-invalid}
+
+function uninstall_runner() {
+  local runnerFolderPattern=${RUNNER_FOLDER_PATTERN:-"action-runner-{id}"}
+
+  find $HOME -maxdepth 1 -type d -iname "$runnerFolderPattern" | while read runnerFolder; do
+    cd $runnerFolder
+
+    sudo ./svc.sh stop
+    sudo ./svc.sh uninstall
+
+    ./config.sh remove --token $GITHUB_TOKEN
+
+    cd $HOME
+
+    rm -rf $runnerFolder
+  done
+}
+
+if [[ "$GITHUB_TOKEN" == "invalid" ]]; then
+    echo "Invalid Github Token. Not mentioned."
+    exit 2
+fi
+
+uninstall_runner
