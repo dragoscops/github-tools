c9f2384c4603bf3fbe5a49877ebfea8942dba99d
Dragos Cirjan <dragos.cirjan@cellebrite.com>
2024-06-25 14:45:52 +0300

    chore: +

--- install-self-hosted-runner.ps1
+++ install-self-hosted-runner.ps1
@@ -65,7 +65,8 @@ function Install-Runner-Windows {
         $runnerName = $RunnerNamePattern -replace "{id}", $i
         $runnerLabels = $RunnerLabelsPattern -replace "{id}", $i
 
-        $runnerFolder = "${env:HOMEDRIVE}${env:HOMEPATH}\${runnerFolder}"
+        # $runnerFolder = "${env:HOMEDRIVE}${env:HOMEPATH}\${runnerFolder}"
+        $runnerFolder = "C:\actions-runner\${runnerFolder}"
 
         Remove-Item -Recurse -Force $runnerFolder -ErrorAction SilentlyContinue
         New-Item -Path $runnerFolder -ItemType Directory | Out-Null
@@ -73,7 +74,7 @@ function Install-Runner-Windows {
 
         Push-Location $runnerFolder
         Write-Host "Configuring runner #${i}..."
-        & .\config.cmd --unattended --url $GithubRepository --token $GithubToken --name $runnerName --labels $runnerLabels
+        & .\config.cmd --url $GithubRepository --token $GithubToken --name $runnerName --labels $runnerLabels --runasservice
 
         # https://docs.github.com/en/actions/hosting-your-own-runners/managing-self-hosted-runners/configuring-the-self-hosted-runner-application-as-a-service?platform=windows
         Start-Service "actions.runner.*"
