46b51cc063bb78502963193bc6983eea7415d40e
Dragos Cirjan <dragos.cirjan@cellebrite.com>
2024-10-21 10:16:32 +0300

    chore: debug on additional labels for Windows 1729494992

--- install-self-hosted-runner.ps1
+++ install-self-hosted-runner.ps1
@@ -41,15 +41,15 @@ i###############################################################################
 ###############################################################################
 
 param(
-    [string]$GithubRepository = "invalid",
-    [string]$GithubToken = "invalid",
-    [int]$RunnerCount = 2,
-    [string]$RunnerFolderPath = "action-runner-{id}",
-    [string]$RunnerNamePattern = "action-runner-{id}",
-    [string]$RunnerLabelsPattern = "action-runner",
-    [string]$RunnerAdditionalLabels = "",
-    [string]$RunnerDownloadUrl = "https://github.com/actions/runner/releases/download/v2.317.0/actions-runner-win-x64-2.317.0.zip",
-    [string]$RunnerDownloadSha = "a74dcd1612476eaf4b11c15b3db5a43a4f459c1d3c1807f8148aeb9530d69826",
+    [string]($GithubRepository = "invalid"),
+    [string]($GithubToken = "invalid"),
+    [int]($RunnerCount = 2),
+    [string]($RunnerFolderPath = "action-runner-{id}"),
+    [string]($RunnerNamePattern = "action-runner-{id}"),
+    [string]($RunnerLabelsPattern = "action-runner"),
+    [string]($RunnerAdditionalLabels = ""),
+    [string]($RunnerDownloadUrl = "https://github.com/actions/runner/releases/download/v2.317.0/actions-runner-win-x64-2.317.0.zip"),
+    [string]($RunnerDownloadSha = "a74dcd1612476eaf4b11c15b3db5a43a4f459c1d3c1807f8148aeb9530d69826"),
     [switch]$Help,
     [switch]$H
 )
