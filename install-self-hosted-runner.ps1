724e932ef380c34dc64632e16c04cf97ebe5d0d3
Dragos Cirjan <dragos.cirjan@cellebrite.com>
2024-10-21 10:12:22 +0300

    chore: debug on additional labels for Windows

--- install-self-hosted-runner.ps1
+++ install-self-hosted-runner.ps1
@@ -50,7 +50,7 @@ param(
     [string]$RunnerAdditionalLabels = "",
     [string]$RunnerDownloadUrl = "https://github.com/actions/runner/releases/download/v2.317.0/actions-runner-win-x64-2.317.0.zip",
     [string]$RunnerDownloadSha = "a74dcd1612476eaf4b11c15b3db5a43a4f459c1d3c1807f8148aeb9530d69826",
-    [swtich]$Help = $false,
+    [switch]$Help = $false,
     [switch]$H = $false
 )
 
